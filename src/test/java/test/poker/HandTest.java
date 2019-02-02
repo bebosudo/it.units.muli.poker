1ed255c34e56d6a06ba8efe70b20874812aa099e
Alberto Chiusole <bebo.sudo@gmail.com>
2019-02-02 10:46:23 +0100

    Update names of scores.

--- src/test/java/test/poker/HandTest.java
+++ src/test/java/test/poker/HandTest.java
@@ -18,7 +18,7 @@ public class HandTest {
     public void TestPairKK(){
         Hand h = new Hand("Kc 9s Ks Ad 4d 3d 2s");
         h.setScore();
-        assertThat(h.getScore(), is(equalTo(CardHands.ONE_PAIR)));
+        assertThat(h.getScore(), is(equalTo(CardHands.PAIR)));
     }
 
 
@@ -26,14 +26,14 @@ public class HandTest {
     public void TestPair33(){
         Hand h = new Hand("9s Ks 3d 8d 7s Ad 3s");
         h.setScore();
-        assertThat(h.getScore(), is(equalTo(CardHands.ONE_PAIR)));
+        assertThat(h.getScore(), is(equalTo(CardHands.PAIR)));
     }
 
     @Test
     public void TestPairAA(){
         Hand h = new Hand("3s 7d 8s Ac Ad Kd 6d");
         h.setScore();
-        assertThat(h.getScore(), is(equalTo(CardHands.ONE_PAIR)));
+        assertThat(h.getScore(), is(equalTo(CardHands.PAIR)));
     }
 
     
