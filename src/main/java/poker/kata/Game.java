977072723d20d8255196c0087e00406096673470
nicolagennaro <nicolagennaro@gmail.com>
2019-02-06 21:30:33 +0100

    Game constructor for string, and relative test

--- src/main/java/poker/kata/Game.java
+++ src/main/java/poker/kata/Game.java
@@ -1,12 +1,23 @@
 package poker.kata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Game {
-    ArrayList<Hand> hands;
 
-    public Game(ArrayList<Hand> hands) {
-        this.hands = hands;
+    private ArrayList<Hand> hands;
+    private ArrayList<Rank> ranks;
+    private boolean[] winners;
+
+    public Game(String[] handsStr) {
+        hands = Arrays.stream(handsStr).map(Hand::new).collect(Collectors.toCollection(ArrayList::new));
+        ranks = hands.stream().map(Hand::getScore).collect(Collectors.toCollection(ArrayList::new));
+
+    }
+
+    public ArrayList<Rank> getRanks(){
+        return ranks;
     }
 
 
--- src/test/java/test/poker/GameTests.java
+++ src/test/java/test/poker/GameTests.java
@@ -3,11 +3,34 @@ package test.poker;
 import org.junit.Test;
 import poker.kata.Game;
 import poker.kata.Hand;
+import poker.kata.Rank;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class GameTests {
 
+    @Test
+    public void ParsingTest() {
 
+        String[] s = {"Kc 9s Ks Kd 9d 3c 6d\n",
+                "9c Ah Ks Kd 9d 3c 6d\n",
+                "Ac Qc Ks Kd 9d 3c\n",
+                "9h 5s\n",
+                "4d 2d Ks Kd 9d 3c 6d\n",
+                "7s Ts Ks Kd 9d\n"};
+
+        Game g = new Game(s);
+
+        Rank[] expRanks = {Rank.FULL_HOUSE, Rank.TWO_PAIRS, Rank.FOLD, Rank.FOLD, Rank.FLUSH, Rank.FOLD};
+        Rank[] ranks = g.getRanks().toArray(new Rank[0]);
+        for (int i = 0; i < s.length; i++) {
+            assertThat(ranks[i], is(equalTo(expRanks[i])));
+        }
+
+    }
 
 }
